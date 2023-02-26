e724866500f3f9011ce01f8fff7ae5f3bbf7b826
aleksst85 <65343095+aleksst85@users.noreply.github.com>
2023-02-26 09:23:34 +0700

    изменения модулей

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+PATH = "candidates.json"
+from util import load_candidates_from_json, get_candidate
 from flask import Flask
 
 
@@ -6,7 +8,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, world!'
+    s='<ol><br>'
+    canditade_list=load_candidates_from_json(PATH)
+    for cand in canditade_list:
 
+        s+=f"<li><a href = '/profile/{cand['id']}'>{cand['name']}</li><br>"
+    
+    s+='</ol>'
+    return s
 
-app.run()
+@app.route('/profile/<int:id>')
+def profile(id):
+    cand = get_candidate(id)
+    s=f"<img src={cand['picture']}><br><h1>{cand['name']}</h1><p><b>Позиция: </b>{cand['position']}</p><p><b>Навыки: </b>{cand['skills']}</p>"
+    return s
+
+app.run(debug=True)
