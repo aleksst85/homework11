3e40012ac230bf759e26a25bc659942101a731ae
aleksst85 <65343095+aleksst85@users.noreply.github.com>
2023-02-26 13:21:30 +0700

    Итог

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 PATH = "candidates.json"
-from util import load_candidates_from_json, get_candidate
+from util import load_candidates_from_json, get_candidate, get_candidates_by_name, get_candidates_by_skill
 from flask import Flask, render_template
 
 
@@ -15,4 +15,19 @@ def profile(id):
     candidate = get_candidate(id)
     return render_template('profile.html', candidate = candidate )
 
-app.run(debug=True)
+
+@app.route('/search/<name>')
+def search(name):
+    candidate_name = get_candidates_by_name(name)
+    return render_template('search.html', candidates = candidate_name, candidates_len=len(candidate_name) )
+
+
+@app.route('/skills/<skill>')
+def search_skills(skill):
+    candidate_skills = get_candidates_by_skill(skill)
+    return render_template('skills.html', candidates = candidate_skills, candidate_len = len(candidate_skills), skill=skill) 
+
+
+
+
+app.run()
--- util.py
+++ util.py
@@ -1,12 +1,14 @@
 import json
 PATH = "candidates.json"
 
+
 def load_candidates_from_json(path):
     with open(path, 'r', encoding ='utf-8') as file:
-        data = json.load(file)
-    return data 
+        _data = json.load(file)
+    return _data 
 
 
+_data=load_candidates_from_json(PATH)
 
 def get_candidate(candidate_id):
     data = load_candidates_from_json(PATH)
@@ -15,27 +17,21 @@ def get_candidate(candidate_id):
             return {'name':cand['name'], "picture":cand["picture"],
                     'position':cand['position'], 'skills':cand['skills']
                     }
-            break
+    return {'not_found':'Ушел на обед'}
 
 
 
 
 def get_candidates_by_name(candidate_name):
-    data = load_candidates_from_json(PATH)
-    for cand in data:
-        if cand["name"] == candidate_name:
-            return cand
-            break
+    return [candidate for candidate in _data if candidate_name.lower() in candidate['name'].lower()]
+
 
     
 def get_candidates_by_skill(skil_name):    
-    data = load_candidates_from_json(PATH)
-    return_list={}
-    for cand in data:
-        skils = cand["skills"].split(", ")
-        skils =[skil.lower() for skil in skils]
-        if skil_name.lower() in skils:
-            return_list[cand['id']]=cand
-    return return_list
-
+    candidates=[]
+    for candidate in _data:
+        skills = candidate['skills'].lower().split(', ')
+        if skil_name.lower() in skills:
+            candidates.append(candidate)
+    return candidates
         
