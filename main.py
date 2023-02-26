af70ddfb1c0e2913d4cb9d26b49624c0f8d643d6
aleksst85 <65343095+aleksst85@users.noreply.github.com>
2023-02-26 10:21:52 +0700

    переделка под шаблонизатор 1 шаблон

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
 PATH = "candidates.json"
 from util import load_candidates_from_json, get_candidate
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
-
+data=load_candidates_from_json(PATH)
 
 @app.route('/')
 def index():
-    s='<ol><br>'
-    canditade_list=load_candidates_from_json(PATH)
-    for cand in canditade_list:
-
-        s+=f"<li><a href = '/profile/{cand['id']}'>{cand['name']}</li><br>"
-    
-    s+='</ol>'
-    return s
+    return render_template('index.html', candidates = data)
 
 @app.route('/profile/<int:id>')
 def profile(id):
